8138d6f4527e948ea0832e8a190422c08c55c025
YFanha <yannfnha@gmail.com>
2025-01-23 14:11:54 +0100

    feat: add errors display after terraform run

--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,6 @@
 import json
 from flask import Flask, render_template, request, redirect
+import subprocess
 
 DATA_FILE = "data.json"
 
@@ -23,20 +24,27 @@ def edit_data():
 def about():
     return render_template('about.html')
 
+def error(error):
+    return render_template('error.html', error=error)
+
 @app.route('/save/', methods=['POST'])
 def save_data():
-    names = request.form.getlist('name')
-    nbrs = request.form.getlist('nbr')
-    client_id = request.form.getlist('id')
+    try:
+        names = request.form.getlist('name')
+        nbrs = request.form.getlist('nbr')
+        client_id = request.form.getlist('id')
 
-    data = {
-        "clients" : [
-            {"id": client_id, "name": name, "nbr_vms": nbr}
-            for client_id, name, nbr in zip(client_id, names, nbrs)
-        ]
-    }
+        data = {
+            "clients" : [
+                {"id": client_id, "name": name, "nbr_vms": nbr}
+                for client_id, name, nbr in zip(client_id, names, nbrs)
+            ]
+        }
 
-    with open(DATA_FILE, 'w') as file:
-        json.dump(data, file, ensure_ascii=False, indent=4)
+        with open(DATA_FILE, 'w') as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
 
-    return redirect("/")
+        subprocess.run(["terraform", "apply", "--auto-approve"], check=True)
+        return redirect("/")
+    except Exception as e:
+        return error(str(e))
